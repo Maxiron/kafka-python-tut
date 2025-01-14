a5ff032dd15694107060a3a648f66253c170ad38
Maxiron <naarons800@gmail.com>
2025-01-14 22:15:44 +0100

    Add delay before running tests to ensure server readiness

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@
 # Run the docker compose file
 docker compose up --build -d
 
+
 # Optional argument to run the test.py file or not (using --runtest)
 if [ "$1" = "--runtest" ]; then
+
+    # Wait for the server to start
+    sleep 5
     python3 test.py
 fi
